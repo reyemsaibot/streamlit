56f0c29487b78b18b3420f0685810ec02b5708eb
Tobias Meyer <reyemsaibot@gmail.com>
2025-10-14 11:57:41 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ from Streamlit1 import object_dependencies, exposed_views, database_tables, obje
 import streamlit as st
 import json
 import pandas as pd
-from df_global_search import DataFrameSearch
+
 from requests_oauthlib import OAuth2Session
 import urllib.parse
 import requests
@@ -337,28 +337,7 @@ def database_tables_gui():
     result = st.empty()
     if st.session_state.display:
         with result.container(width=2000, border=True):    
-
-
-            search_bar_columns = st.columns((2, 1, 0.5, 0.75, 1))
-            with search_bar_columns[1]:
-                search_text = st.text_input(
-                    "Search", label_visibility="collapsed", placeholder="Search Text"
-                )
-            with search_bar_columns[2]:
-                is_regex = st.toggle("Regex", value=False)
-            with search_bar_columns[3]:
-                case_sensitive = st.toggle("Case Sensitive", value=False)
-            with search_bar_columns[4]:
-                highlight_match = st.toggle("Highlight Matching Cells", value=True)
-
-            with DataFrameSearch(
-                dataframe=st.session_state.df,
-                text_search=search_text,
-                case_sensitive=case_sensitive,
-                regex_search=is_regex,
-                highlight_matches=highlight_match,
-            ) as df:
-                st.dataframe(data=df, use_container_width=True, hide_index=True)
+            st.dataframe(data=st.session_state.df, use_container_width=True, hide_index=True)
             st.session_state.display = False
 
 def userlist_gui():
